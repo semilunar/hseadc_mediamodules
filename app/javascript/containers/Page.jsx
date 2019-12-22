41ec789e87afe287b170f0393376af906cff87be
semilunar <katyanikitina.me@yandex.ru>
2019-12-23 00:41:33 +0300

    added slider-story, fixed order of components

--- app/javascript/containers/Page.jsx
+++ app/javascript/containers/Page.jsx
@@ -6,51 +6,16 @@ import Split from '../presets/Split'
 import Flip from '../presets/Flip'
 import Scroll from '../presets/Scroll'
 import CustomVideo from '../presets/CustomVideo'
-
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-
-import MagicSliderDots from 'react-magic-slider-dots'
+import SliderDots from '../presets/SliderDots'
+import SliderArrows from '../presets/SliderArrows'
+import SliderStory from '../presets/SliderStory'
 
 import Draggable, { DraggableCore } from 'react-draggable'
 
 import Symbol from '../../assets/images/symbol.svg'
 
-import Slider1 from '../../assets/images/slider1.jpg'
-import Slider2 from '../../assets/images/slider2.jpg'
-import Slider3 from '../../assets/images/slider3.jpg'
-import Slider4 from '../../assets/images/slider4.jpg'
-import Slider5 from '../../assets/images/slider5.jpg'
-import Slider6 from '../../assets/images/slider6.jpg'
-import Slider7 from '../../assets/images/slider7.jpg'
-import Slider8 from '../../assets/images/slider8.jpg'
-
 export default class Page extends React.Component {
   render() {
-    const settingsDots = {
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      appendDots: dots => {
-        return <MagicSliderDots dots={dots} numDotsToShow={3} dotWidth={30} />
-      }
-    }
-    const settingsArrows = {
-      dots: false,
-      arrows: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      appendDots: dots => {
-        return <MagicSliderDots dots={dots} numDotsToShow={3} dotWidth={30} />
-      }
-    }
-
     const dragStyle = {
       backgroundImage: `url(${Symbol}`
     }
@@ -60,47 +25,15 @@ export default class Page extends React.Component {
           <div className="draggable" style={dragStyle}></div>
         </Draggable>
         <VideoMask />
-        <HorizontalScroll />
+        <CustomVideo />
+        <Scroll />
+        <SliderDots />
+        <SliderArrows />
         <Split />
+        <HorizontalScroll />
         <Flip />
 
-        <div className="Preset Slider Dots">
-          <Slider {...settingsDots}>
-            <div>
-              <img src={Slider1} className="slider-img" />
-            </div>
-            <div>
-              <img src={Slider2} className="slider-img" />
-            </div>
-            <div>
-              <img src={Slider3} className="slider-img" />
-            </div>
-          </Slider>
-        </div>
-
-        <div className="Preset Slider Arrows">
-          <Slider {...settingsArrows}>
-            <div>
-              <img src={Slider4} className="slider-img" />
-            </div>
-            <div>
-              <img src={Slider5} className="slider-img" />
-            </div>
-            <div>
-              <img src={Slider6} className="slider-img" />
-            </div>
-            <div>
-              <img src={Slider7} className="slider-img" />
-            </div>
-            <div>
-              <img src={Slider8} className="slider-img" />
-            </div>
-          </Slider>
-        </div>
-
-        <Scroll />
-
-        <CustomVideo />
+        <SliderStory />
       </div>
     )
   }
