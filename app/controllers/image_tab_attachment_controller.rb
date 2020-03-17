888db54b79da25d732568a4bbe33512e947b3476
semilunar <katyanikitina.me@yandex.ru>
2020-03-17 10:31:31 +0300

    redirect in controller

--- app/controllers/image_tab_attachment_controller.rb
+++ app/controllers/image_tab_attachment_controller.rb
@@ -3,5 +3,12 @@ class ImageTabAttachmentController < ApplicationController
     newImage = ImageTabAttachment.new
     newImage.link = params[:image]
     newImage.save
+
+    if newImage.save
+      redirect_to root_path
+    else
+      puts "something goes wrong"
+      redirect_to :back
+    end
   end
 end
